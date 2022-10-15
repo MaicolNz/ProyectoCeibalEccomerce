f67afde3cc175a0d331fed1ab7dba99877dbc52d
MaicolNz <maicolnunez3@gmail.com>
2022-10-15 15:07:56 -0300

    "carrito completo"

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,8 @@
+var ids = [];
+var costs = [];
+function subtotal(cost, cant) {
+  return cost * cant;
+}
 fetch("https://japceibal.github.io/emercado-api/user_cart/" + "25801" + ".json")
   .then((response) => response.json())
   .then(function (data) {
@@ -12,10 +17,25 @@ fetch("https://japceibal.github.io/emercado-api/user_cart/" + "25801" + ".json")
           <th><img src="${producto.images[0]}" width=50px></th>
           <td>${producto.name}</td>
           <td>${producto.currency} ${producto.cost}</td>
-          <td><input type="number" id="quantity" name="quantity" min="1" max="10" value="1"></td>
-          <td> ${producto.currency} ${producto.cost}</td>
+          <td><input type="number" id="quantity${
+            producto.id
+          }" name="quantity" min="1" max="10" value="1"></td>
+          <td id="sub${producto.id}"> ${producto.currency} ${subtotal(
+            producto.cost,
+            1
+          )}</td>
           </tr>`;
+          ids.push(producto.id);
+          costs.push(producto.cost);
           document.getElementById("tabla").innerHTML += html;
         });
     }
   });
+
+document.addEventListener("input", function (evento) {
+  for (let i = 0; i < ids.length; i++) {
+    document.getElementById("sub" + ids[i]).innerHTML = `$ ${
+      document.getElementById("quantity" + ids[i]).value * costs[i]
+    }`;
+  }
+});
