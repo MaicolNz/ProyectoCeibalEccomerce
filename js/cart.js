985ec06d2f1887cc24cd5ea164e25f393d1a5a1f
MaicolNz <maicolnunez3@gmail.com>
2022-10-30 14:49:02 -0300

    Precios
    Agregados pos precios in real time

--- js/cart.js
+++ js/cart.js
@@ -27,6 +27,15 @@ fetch("https://japceibal.github.io/emercado-api/user_cart/" + "25801" + ".json")
           </tr>`;
           ids.push(producto.id);
           document.getElementById("tabla").innerHTML += html;
+          document.getElementById("subM").innerHTML = `${
+            producto.currency
+          } ${subtotal(producto.cost, 1)}`;
+          document.getElementById("EnvioM").innerHTML = `${producto.currency} ${
+            (5 / 100) * subtotal(producto.cost, 1)
+          }`;
+          document.getElementById("TotalM").innerHTML = `${producto.currency} ${
+            subtotal(producto.cost, 1) + (5 / 100) * subtotal(producto.cost, 1)
+          }`;
         });
     }
   });
@@ -36,11 +45,37 @@ document.addEventListener("input", function (evento) {
     fetch(PRODUCT_INFO_URL + ids[i] + ".json")
       .then((response) => response.json())
       .then(function (producto) {
+        var total =
+          document.getElementById("quantity" + ids[i]).value * producto.cost;
+        var totalV;
+        document.getElementById("subM").innerHTML = `${producto.currency} ${
+          document.getElementById("quantity" + ids[i]).value * producto.cost
+        }`;
         document.getElementById("sub" + producto.id).innerHTML = `${
           producto.currency
         } ${
           document.getElementById("quantity" + ids[i]).value * producto.cost
         }`;
+        //Envios//
+        if (document.getElementById("premium").checked) {
+          document.getElementById("EnvioM").innerHTML = `${
+            producto.currency
+          } ${Math.round((15 / 100) * total)}`;
+          totalV = Math.round((15 / 100) * total);
+        } else if (document.getElementById("express").checked) {
+          document.getElementById("EnvioM").innerHTML = `${
+            producto.currency
+          } ${Math.round((7 / 100) * total)}`;
+          totalV = Math.round((7 / 100) * total);
+        } else if (document.getElementById("standard").checked) {
+          document.getElementById("EnvioM").innerHTML = `${
+            producto.currency
+          } ${Math.round((5 / 100) * total)}`;
+          totalV = Math.round((5 / 100) * total);
+        }
+        document.getElementById("TotalM").innerHTML = `${producto.currency} ${
+          total + totalV
+        }  `;
       });
   }
 });
